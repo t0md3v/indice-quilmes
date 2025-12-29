a43d82b5e2b252e203d4d4bd2a3e2f3ca3ace29c
t0md3v <t0md3v@users.noreply.github.com>
2025-12-29 00:10:43 -0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/**window.setInterval("reloadIFrame();", 5000);
+
+     function reloadIFrame()
+     {
+          document.frames["frameNameHere"].location.reload();
+     } **/
+// Interval is 5 sec 
